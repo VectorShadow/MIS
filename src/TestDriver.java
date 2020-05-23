03194086b4040e5b895d0bf792a8e44465a0ff6f
VectorShadow <ncbrayshaw@hotmail.com>
2020-05-23 20:06:15 +0430

    Added quick scale and helper methods, and more tests.

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -14,14 +14,40 @@ public class TestDriver {
 
     public static void main(String[] args) throws IOException {
         BufferedImage sourceImage = ImageIO.read(new File(SOURCE_PATH));
+
+        //set maps test
         long start = System.currentTimeMillis();
         ScalingFunctions.setMaps(sourceImage.getHeight(), sourceImage.getWidth(), TARGET_HEIGHT, TARGET_WIDTH);
         long stop = System.currentTimeMillis();
         System.out.println("Set maps in " + (stop - start) + " ms.");
+
+        //scale with preset maps test
         start = System.currentTimeMillis();
-        BufferedImage targetImage = ScalingFunctions.scale(sourceImage);
+        BufferedImage targetImage = ScalingFunctions.mapScale(sourceImage);
         stop = System.currentTimeMillis();
-        System.out.println("Scaled image in " + (stop - start) + " ms.");
+        System.out.println("Scaled image with preset maps in " + (stop - start) + " ms.");
         ImageIO.write(targetImage, PNG, new File(TARGET_PATH));
+
+        //scale without preset maps test
+        start = System.currentTimeMillis();
+        targetImage = ScalingFunctions.quickScale(sourceImage, TARGET_HEIGHT, TARGET_WIDTH);
+        stop = System.currentTimeMillis();
+        System.out.println("Scaled image without preset maps in " + (stop - start) + " ms.");
+
+        //repeat scaling with preset maps test
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 64; ++i) {
+            targetImage = ScalingFunctions.mapScale(sourceImage);
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("Scaled image with preset maps repeatedly in " + (stop - start) + " ms.");
+
+        //repeat scaling without preset maps test
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 64; ++i) {
+            targetImage = ScalingFunctions.quickScale(sourceImage, TARGET_HEIGHT, TARGET_WIDTH);
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("Scaled image without preset maps repeatedly in " + (stop - start) + " ms.");
     }
 }
